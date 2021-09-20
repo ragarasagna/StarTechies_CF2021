50f6876fdae00d57c5fbefdcd218b997dac1bbcc
harshitalokinendi <harshilokinendi@gmail.com>
2021-09-20 19:45:59 +0530

    RegistrationBranch-initial_commit

---
+++ BugTrackingSystemApplication/src/com/hsbc/beans/Users.java
@@ -0,0 +1,35 @@
+package com.hsbc.beans;
+
+import java.sql.Timestamp;
+
+public class Users extends ImportedUsers{
+	private String password;
+	private Timestamp lastLogin;
+	int projectCounter;
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(Timestamp lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public int getProjectCounter() {
+		return projectCounter;
+	}
+
+	public void setProjectCounter(int projectCounter) {
+		this.projectCounter = projectCounter;
+	}
+
+}
