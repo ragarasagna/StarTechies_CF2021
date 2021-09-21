cb5164082cda5f6d489af03b34a9cff1d39e7afb
Chaitanya Ashtekar <chaitanya.ashtekar17@vit.edu>
2021-09-21 19:28:21 +0530

    Bugs class

---
+++ BugTrackingSystemApplication/src/com/hsbc/beans/Bugs.java
@@ -0,0 +1,12 @@
+package com.hsbc.beans;
+
+import java.time.LocalDate;
+
+public class Bugs {
+    private String bugId,bugTitle,bugDesc,projectId,createdBy,
+    assignedTo,closedBy,bugStatus,severityLevel;
+    private LocalDate openDate;
+    
+    
+    
+}
